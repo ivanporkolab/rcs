d9c2bfac5d5449f26ae25ccc035037658cb3ee34
Ivan <ivan.porkolab@undabot.com>
2014-11-21 18:14:24 +0100

    initial commit, empty app

---
+++ RCS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RCS
+//
+//  Created by ivan porkolab on 21/11/14.
+//  Copyright (c) 2014 fragment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RCS/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RCS
+//
+//  Created by ivan porkolab on 21/11/14.
+//  Copyright (c) 2014 fragment. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
